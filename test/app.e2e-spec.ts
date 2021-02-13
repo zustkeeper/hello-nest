635882111c1c27d92b75563f6fa569e2f88acc6e
zustkeeper <zustkeeper@naver.com>
2021-02-14 05:52:15 +0900

    e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +26,59 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Game API');
+  });
+
+  describe('/games', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/games').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/games')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/games')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'Other',
+        })
+        .expect(400);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/games').expect(404);
+    });
+  });
+
+  describe('/games/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/games/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/games/999').expect(404);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/games/1')
+        .send({
+          title: 'Updated Test',
+        })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/games/1').expect(200);
+    });
   });
 });
