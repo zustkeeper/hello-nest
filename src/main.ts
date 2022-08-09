87642a4a15eb3c89be30f0553b565cee340497e5
Byungjoo Kim <52707622+zustkeeper@users.noreply.github.com>
2022-08-09 17:39:40 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
